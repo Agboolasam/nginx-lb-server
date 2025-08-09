b261511312acc21f6145d879f362cc0b68fb3c9d
Agboola Samson <agboolasamson602@gmail.com>
2025-08-09 22:25:51 +0100

    Updated server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const appName = process.env.APP_NAME;
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  console.log(`Serving from ${appName}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${Port}`);
+});
